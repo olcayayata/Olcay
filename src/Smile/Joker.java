fc1a60d2ce3d52bbb14d12bc161e38bbb3e299e0
olcayayata <olcayayata4@gmail.com>
2021-08-03 21:43:40 -0400

    Olcay'in Kucuk Dunyasi

--- src/Smile/Joker.java
+++ src/Smile/Joker.java
@@ -3,5 +3,6 @@ package Smile;
 public class Joker {
     public static void main(String[] args) {
         System.out.println("happy wife happy life!");
+        System.out.println("Smile to the World!");
     }
 }
